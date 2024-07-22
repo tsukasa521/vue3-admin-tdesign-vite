d6bcbea48edc2c4f6e881f30e9e24a7955922421
zhoukan <kkplusplus@163.com>
2024-07-22 17:25:16 +0800

    feat: 添加iconfont页

--- src/views/icon-view/IconView.vue
+++ src/views/icon-view/IconView.vue
@@ -3,9 +3,9 @@
     <div class="page-inner">
       <t-space>
         <i class="iconfont icon-notification text-primary" style="font-size: 48px;" />
-        <i class="iconfont icon-notification-filled text-success" style="font-size: 48px;" />
+        <i class="iconfont icon-notification-filled text-success" style="font-size: 36px;" />
         <i class="iconfont icon-pause-circle text-danger" style="font-size: 48px;" />
-        <i class="iconfont icon-pause-circle-filled text-regular" style="font-size: 48px;" />
+        <i class="iconfont icon-pause-circle-filled text-regular" style="font-size: 36px;" />
         <i class="iconfont icon-lock-off text-secondary" style="font-size: 48px;" />
       </t-space>
     </div>
@@ -16,4 +16,4 @@
 
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped></style>
